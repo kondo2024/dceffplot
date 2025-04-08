d340d1969e14cb554d4f15d022a0a74387a8fd23
kondo <kondo@ribf.riken.jp>
2025-04-08 18:29:51 +0900

    updatye

---
+++ sources/TDCEffPlot.hh
@@ -0,0 +1,16 @@
+#ifndef TDCEFFPLOT_HH
+#define TDCEFFPLOT_HH
+
+#include "TObject.h"
+
+class TDCEffPlot : public TObject
+{
+public:
+  TDCEffPlot(){;}
+  virtual ~TDCEffPlot(){;}
+
+public:
+  ClassDef(TDCEffPlot, 1)
+};
+
+#endif
